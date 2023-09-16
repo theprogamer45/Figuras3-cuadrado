8745170edc3246873c624e69771b7586221eaac5
theprogamer45 <juanpgh21@gmail.com>
2023-09-15 23:44:12 -0500

    cuadrado

---
+++ src/poo/pkg3/ejercicio/figuras/geometricas/POO3EjercicioFigurasGeometricas.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package poo.pkg3.ejercicio.figuras.geometricas;
+
+/**
+ *
+ * @author juanp
+ */
+public class POO3EjercicioFigurasGeometricas {
+
+    
+    public static void main(String[] args) {
+        Cuadrado form = new Cuadrado();
+        form.setVisible(true);
+    }
+    
+}
